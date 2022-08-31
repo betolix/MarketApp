06ba0e6632c46e8be6f5b35739db056917ccd8e5
beto <betolix@gmail.com>
2022-08-31 08:59:51 -0400

    Se automatizó la espera de la pantalla splash con un Handler y el salto a la pantalla del login.

--- app/src/main/java/io/h3llo/appmarket/SplashFragment.kt
+++ app/src/main/java/io/h3llo/appmarket/SplashFragment.kt
@@ -1,6 +1,8 @@
 package io.h3llo.appmarket
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +15,8 @@ class SplashFragment : Fragment() {
 
     private var _binding : FragmentSplashBinding? = null
     private val binding get() = _binding!!
+
+    val SPLASH_TIME_OUT:Long = 3000
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -28,7 +32,11 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Navigation.findNavController(view).navigate(R.id.action_splashFragment_to_loginFragment)
+        Handler(Looper.getMainLooper()).postDelayed({
+            Navigation.findNavController(view).navigate(R.id.action_splashFragment_to_loginFragment)
+        },SPLASH_TIME_OUT)
+
+
     }
 
 }
