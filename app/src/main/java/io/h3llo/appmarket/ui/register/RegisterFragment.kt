913108e020af43773891adc7a009c1887f828bc3
beto <betolix@gmail.com>
2022-09-06 12:24:31 -0400

    Se arregla bug en el que la app hace crash al momento de pasar del LoginFragment al RegisterFragment. Error se generó al crear el paquete ui/register y mover el fragmento a esa ubicación. Se soluciona actualizando la referencia en el tablero de navegacion (en el XML).

--- app/src/main/java/io/h3llo/appmarket/ui/register/RegisterFragment.kt
+++ app/src/main/java/io/h3llo/appmarket/ui/register/RegisterFragment.kt
@@ -5,55 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import io.h3llo.appmarket.R
+import io.h3llo.appmarket.databinding.FragmentRegisterBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding : FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel : RegisterViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+        events()
+    }
+
+    private fun init() {
+        // OBTENER LOS GENEROS EN EL VIEWMODEL DE LA CLASE
+        viewModel.obtenerGeneros()
+    }
+
+    private fun events() = with(binding) {
+        btnCrearCuenta.setOnClickListener{
+            crearCuenta()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    private fun crearCuenta() = with(binding){
+        val nombres = edtNombres.editText?.text.toString()
+        val apellidos = edtApellidos.editText?.text.toString()
+        val correo = edtCorreoElectronico.editText?.text.toString()
+        val celular = edtCelular.editText?.text.toString()
+        val numeroDocumento = edtNumeroDocumento.editText?.text.toString()
+        val clave = edtClave.editText?.text.toString()
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
+
 }
---
+++ app/src/main/java/io/h3llo/appmarket/ui/register/RegisterViewModel.kt
@@ -0,0 +1,24 @@
+package io.h3llo.appmarket.ui.register
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class RegisterViewModel : ViewModel(){
+    fun obtenerGeneros() {
+
+        viewModelScope.launch {
+             try {
+
+             }catch (ex:Exception){
+
+             }finally {
+
+             }
+        }
+
+    }
+
+
+}
