d7d7d3c1a851c1ef3f82aa6e347f56e1eda65039
beto <betolix@gmail.com>
2022-09-13 22:43:36 -0400

    Ordenamiento de código

---
+++ app/src/main/java/io/h3llo/appmarket/ui/cart/MisComprasFragment.kt
@@ -0,0 +1,19 @@
+package io.h3llo.appmarket.ui.cart
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.h3llo.appmarket.R
+
+
+class MisComprasFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mis_compras, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/io/h3llo/appmarket/ui/categories/CategoriesFragment.kt
@@ -0,0 +1,20 @@
+package io.h3llo.appmarket.ui.categories
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.h3llo.appmarket.R
+
+
+class CategoriesFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_categories, container, false)
+    }
+
+
+}
